6f2d313b76672f6365975e7b2cabef537fc76d3a
Matthew Havelock <mjhavelock@gmail.com>
2012-10-29 11:47:43 +0000

    mini cart updates and chankout

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ jQuery(function() {
 
 	html5placeholder();
 	showHide('cartPanel', 'cartBtn');
-	showHide2('search_mini_form', 'searchReveal');
+	//showHide2('search_mini_form', 'searchReveal');
 	showHide2('fx-converter', 'currencyConvertor');
 	showHide2('ship-estimate', 'ship-estimate-open');
 	menuCurrentHighlight();
